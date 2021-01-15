9bb7048aa141ff89c814b89bbc0694dbced3a8d8
Mariano <marianoacosta.003@gmail.com>
2021-01-15 17:28:23 -0300

    refactoring

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Huffman-Encoder-for-Nodejs
 ## Author: Mariano L. Acosta
+
+# Description
+
+# Installation
+
+        npm install --save huffman-url-compressor
+
+# Usage
+        //create encoder
+
+        let Encoder = createEncoder(train);
+
+        //create a base64 encoded stream
+
+        let encodedParam = encodeConfig(test.trim(),Encoder)
+        
+        //retrieve original from encoded param
+
+        let decodConfig = decodeConfig(encodedParam,Encoder)
+
--- huffman.js
+++ huffman.js
@@ -1,18 +1,18 @@
 // Huffman encoder for shorter urls
 // Author: Mariano L. Acosta
 
-const { train, test } = require('./dataset.js');
+//const { train, test } = require('./dataset.js');
 const { decodeConfig , encodeConfig, createEncoder } = require('./utils.js');
 
 //create encoder
 
-let Encoder = createEncoder(train);
+//let Encoder = createEncoder(train);
 
 //create encoded base64 stream
 
-let encodedParam = encodeConfig(test.trim(),Encoder)
+//let encodedParam = encodeConfig(test.trim(),Encoder)
 
-let buff = new Buffer(test);
+//let buff = new Buffer(test);
 
 //console.log('original:\n');
 //console.log(buff.toString('base64'),'\n');
@@ -21,11 +21,11 @@ let buff = new Buffer(test);
 //console.log(encodedParam,'\n');
 //retrieve config from encoded param
 
-let decodConfig = decodeConfig(encodedParam,Encoder)
+//let decodConfig = decodeConfig(encodedParam,Encoder)
 
 
 //console.log((1 - encodedParam.length/buff.toString('base64').length)*100);
 
 module.exports = {
-    createEncoder,encodeConfig,decodConfig
+    createEncoder,encodeConfig,decodeConfig
 }
